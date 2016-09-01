96a9cc50f5a9c58524db29c3618a404d749d09e4
Ivo Maixner <ivo.maixner@gmail.com>
2016-09-01 11:49:43 +0200

    Add react-router.

---
+++ src/AppRouter.js
@@ -0,0 +1,20 @@
+import React from "react"
+import {Router, Route, Redirect, Link, useRouterHistory} from 'react-router'
+import {createHashHistory} from 'history'
+import App from "./layout/App"
+import RegisterPage from "./user/RegisterPage"
+import LoginPage from "./user/LoginPage"
+
+const appHistory = useRouterHistory(createHashHistory)({queryKey: false})
+
+const AppRouter = () => (
+	<Router history={appHistory}>
+		<Route path="/" component={App}>
+			<Route path="/register" component={RegisterPage}/>
+			<Route path="/login" component={LoginPage}/>
+			<Redirect from="*" to="/"/>
+		</Route>
+	</Router>
+)
+
+export default AppRouter
